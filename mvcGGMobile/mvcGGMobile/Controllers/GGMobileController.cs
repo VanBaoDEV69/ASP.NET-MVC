865c39c51a5266d8048da75c829ba0be41e8597a
VanBaoDEV69 <vanbaopmdp@gmail.com>
2023-03-14 21:25:08 +0700

    Upload Project

---
+++ mvcGGMobile/mvcGGMobile/Controllers/GGMobileController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvcGGMobile.Models;
+
+namespace mvcGGMobile.Controllers
+{
+    public class GGMobileController : Controller
+    {
+        //database
+        dbGGMobileDataContext data = new dbGGMobileDataContext();
+
+        private List<SanPham> LaySanphammoi (int count)
+        {
+            return data.SanPhames.OrderByDescending(a => a.Ngaycapnhat).Take(count).ToList();
+        }
+        // GET: GGMobile
+        public ActionResult Index()
+        {
+            var sanphammoi = LaySanphammoi(5);
+            return View(sanphammoi);
+        }
+    }
+}
